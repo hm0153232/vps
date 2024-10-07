f9d1728456cef1c9ff36320af949de90a8a07b9a
hm0153232 <144235751+hm0153232@users.noreply.github.com>
2024-10-07 21:00:33 +0500

    Update VpnRun.py

--- VpnRun.py
+++ VpnRun.py
@@ -46,6 +46,11 @@ class CRDSetup:
     @staticmethod
     def installDesktopEnvironment():
         os.environ["DEBIAN_FRONTEND"] = "noninteractive"  # Set noninteractive mode
+
+        # Preconfigure keyboard settings to avoid prompts
+        os.system("echo 'keyboard-configuration keyboard-configuration/layout select us' | sudo debconf-set-selections")
+        os.system("echo 'keyboard-configuration keyboard-configuration/model select Generic 101-key PC' | sudo debconf-set-selections")
+
         os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
         os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
         os.system("apt remove --assume-yes gnome-terminal")
@@ -58,10 +63,8 @@ class CRDSetup:
         os.system("echo 'XKBLAYOUT=\"us\"' | sudo tee /etc/default/keyboard")
         os.system("sudo dpkg-reconfigure keyboard-configuration")
 
-        print("Installed XFCE4 Desktop Environment and set default keyboard layout!")
+    print("Installed XFCE4 Desktop Environment and set default keyboard layout!")
 
-
-        
     @staticmethod
     def installGoogleChrome():
         subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"])
